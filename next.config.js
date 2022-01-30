1056b9f8e123060d20c48390ea5b84d35731e611
palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2022-01-30 10:32:55 +0530

    ignore typeerror

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     formats: ["image/avif", "image/webp"],
   },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
