a4a25f55113184c2bac5d054791d30bcb82ba2f3
Dancing Coconut <dancingcoconut12@gmail.com>
2022-02-04 10:53:13 +0530

    Blog+Comments Layout

--- components/comments/Comments.tsx
+++ components/comments/Comments.tsx
@@ -2,12 +2,21 @@ import styles from "./Comments.module.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import CommentIcon from '@mui/icons-material/Comment';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 interface Props{
     date:string;
     author:string;
     content:string;
 }
 function Comments(props:Props){
+    function display(){
+        if(document.getElementById("Add__Comment").style.display === "none"){
+            document.getElementById("Add__Comment").style.display = "flex";
+        }else{
+            document.getElementById("Add__Comment").style.display = "none";
+        }
+    }
     return(
         <div className={styles.Comment__box}>
             <div className={styles.Author__section}>
@@ -21,6 +30,20 @@ function Comments(props:Props){
             <div className={styles.Vote__section}>
                 <ThumbUpIcon className={styles.Up__icon}/>
                 <ThumbDownIcon className={styles.Down__icon}/>
+                <KeyboardArrowDownIcon className={styles.Dropdown}/>
+                <button type="button"className={styles.Reply__button} onClick={display}>
+                    <CommentIcon className={styles.Reply__icon}/>
+                </button>
+            </div>
+            <div className={styles.Comment__write} id="Add__Comment">
+                <AccountCircleIcon className={styles.Account__icon} />
+                <div className={styles.Comm__container}>
+                    <textarea placeholder="Reply" cols="50" rows="5" className={styles.Write__comment}></textarea>
+                    <div className={styles.Comment__options}>
+                        <button className={styles.Comment__submit}>Submit</button>
+                        <button className={styles.Comment__preview}>Preview</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
