95e3652f1933bb4d508e1540c530c530fba20139
palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2022-03-02 15:35:12 +0530

    auth support for signup and login added in frontend

--- components/home/mainsection/Login.tsx
+++ components/home/mainsection/Login.tsx
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Login.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Token } from "@mui/icons-material";
 
 interface Props {}
 
 function Login(props: Props) {
   const [focuspassword, setFocusStatePassword] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [jwtToken, setJwtToken] = useState<String | null>(null);
+  const [userData, setUserData] =
+    useState<{ UserName: String; User_Email: String; User_ID: String }>();
   const { data: session, status } = useSession();
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
+  const loginHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const userLoginData = {
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value,
+    };
+
+    console.log(emailRef.current?.value)
+
+    fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/v1/users/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userLoginData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoggedIn(true);
+        setJwtToken(data.token);
+        setUserData({
+          UserName: data.data.user_data.UserName,
+          User_Email: data.data.user_data.User_Email,
+          User_ID: data.data.user_data["BIN_TO_UUID(id)"],
+        });
+      });
+  };
+  console.log("userData", userData,Token);
   return (
     <div className={styles["login-content"]}>
       {!session && (
         <>
-          <form className={styles["login"]}>
+          <form className={styles["login"]} onSubmit={loginHandler}>
             <h1 className={styles["login-title"]}>Login</h1>
             <div className={styles["login-input"]}>
               <input
@@ -34,7 +68,7 @@ function Login(props: Props) {
                 }}
               />
             </div>
-            <button className={styles["login-button"]} type="button">
+            <button className={styles["login-button"]} type="submit">
               Login
             </button>
           </form>
